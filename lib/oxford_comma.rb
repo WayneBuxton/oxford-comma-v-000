30b01a14a2b843acbd9c89e234a99525e9e5f0d0
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-02 17:28:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
+  array.join
   if array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
-    last_word = "and " + array.pop
-    array << last_word
-    array.join(", ")
   end
 end
